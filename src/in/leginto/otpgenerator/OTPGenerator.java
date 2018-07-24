bce14fe7983543dbc43ec4829753a74d7da4caa7
leginto <leginto.r@gmail.com>
2018-07-24 23:03:37 +0530

    updated source

--- src/in/leginto/otpgenerator/OTPGenerator.java
+++ src/in/leginto/otpgenerator/OTPGenerator.java
@@ -1,10 +1,32 @@
 package in.leginto.otpgenerator;
 
+import java.util.Random;
+
 public class OTPGenerator {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		OTP otp = new OTP();
+		
+		System.out.println(otp.generateOTP(4));
 	}
 
+}
+
+class OTP
+{
+	private final String passwordData = "0123456789";
+	public int generateOTP(int length)
+	{
+		Random ran = new Random();
+		StringBuffer sb = new StringBuffer();
+		
+		for(int i = 0; i<length; i++)
+		{
+			sb.append(passwordData.charAt(ran.nextInt(passwordData.length())));
+		}
+		
+		return Integer.parseInt(sb.toString());
+		
+	}
 }
